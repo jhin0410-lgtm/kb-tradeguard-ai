36d600c0eb2df17cc3173abf26f112581730ea82
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-26 01:14:12 +0900

    Add financial intelligence package

---
+++ src/intelligence/__init__.py
@@ -0,0 +1,5 @@
+"""Transparent intelligence layers built on validated provider data."""
+
+from .financial_health import FinancialHealthResult, analyze_financial_health
+
+__all__ = ["FinancialHealthResult", "analyze_financial_health"]
