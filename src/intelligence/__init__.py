ac76b0bc79bce1be0e1ff6021c6fe354e6454c7f
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-26 01:26:10 +0900

    Export financial trend analysis

--- src/intelligence/__init__.py
+++ src/intelligence/__init__.py
@@ -1,5 +1,11 @@
 """Transparent intelligence layers built on validated provider data."""
 
 from .financial_health import FinancialHealthResult, analyze_financial_health
+from .financial_trends import FinancialTrendResult, analyze_financial_trends
 
-__all__ = ["FinancialHealthResult", "analyze_financial_health"]
+__all__ = [
+    "FinancialHealthResult",
+    "FinancialTrendResult",
+    "analyze_financial_health",
+    "analyze_financial_trends",
+]
