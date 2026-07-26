712af4b89759978d2875b3f0d6e41ff7863675e8
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-26 19:41:38 +0900

    Export trade document screening rules

--- src/intelligence/__init__.py
+++ src/intelligence/__init__.py
@@ -8,14 +8,24 @@ from .country_compliance import (
 )
 from .financial_health import FinancialHealthResult, analyze_financial_health
 from .financial_trends import FinancialTrendResult, analyze_financial_trends
+from .trade_document_rules import (
+    build_document_risk_signals,
+    evaluate_trade_document,
+    load_trade_document_rule_registry,
+    reviewed_terms_from_document,
+)
 
 __all__ = [
     "FinancialHealthResult",
     "FinancialTrendResult",
     "analyze_financial_health",
     "analyze_financial_trends",
+    "build_document_risk_signals",
     "build_fatf_country_fact",
     "build_fatf_country_screening",
     "build_world_bank_country_facts",
+    "evaluate_trade_document",
     "load_fatf_registry",
+    "load_trade_document_rule_registry",
+    "reviewed_terms_from_document",
 ]
