9eed239aace3f332c8b68d706cddb71137cc788c
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-26 20:18:04 +0900

    Export trade finance product matching

--- src/intelligence/__init__.py
+++ src/intelligence/__init__.py
@@ -17,6 +17,15 @@ from .document_reconciliation import (
 )
 from .financial_health import FinancialHealthResult, analyze_financial_health
 from .financial_trends import FinancialTrendResult, analyze_financial_trends
+from .product_matching import (
+    ProductMatchingOutcome,
+    ProductMatchingResult,
+    TradeFinanceNeedProfile,
+    apply_product_matching,
+    canonical_bank_name,
+    load_product_registry,
+    match_trade_finance_products,
+)
 from .trade_document_assessment import (
     TradeDocumentScreeningOutcome,
     apply_trade_document_screening,
@@ -34,20 +43,27 @@ __all__ = [
     "DocumentReconciliationResult",
     "FinancialHealthResult",
     "FinancialTrendResult",
+    "ProductMatchingOutcome",
+    "ProductMatchingResult",
     "ReconciliationPolicy",
     "TradeDocumentScreeningOutcome",
+    "TradeFinanceNeedProfile",
     "analyze_financial_health",
     "analyze_financial_trends",
     "apply_document_reconciliation",
+    "apply_product_matching",
     "apply_trade_document_screening",
     "build_document_risk_signals",
     "build_fatf_country_fact",
     "build_fatf_country_screening",
     "build_world_bank_country_facts",
+    "canonical_bank_name",
     "evaluate_trade_document",
     "load_fatf_registry",
+    "load_product_registry",
     "load_reconciliation_registry",
     "load_trade_document_rule_registry",
+    "match_trade_finance_products",
     "reconcile_trade_documents",
     "reviewed_terms_from_document",
 ]
