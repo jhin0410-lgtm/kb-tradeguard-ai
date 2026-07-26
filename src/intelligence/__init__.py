adc30a103aad8c3bd57bd40787aebbc723dfa1fe
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-26 19:59:15 +0900

    Export document reconciliation layer

--- src/intelligence/__init__.py
+++ src/intelligence/__init__.py
@@ -6,6 +6,15 @@ from .country_compliance import (
     build_world_bank_country_facts,
     load_fatf_registry,
 )
+from .document_reconciliation import (
+    DocumentComparisonResult,
+    DocumentReconciliationOutcome,
+    DocumentReconciliationResult,
+    ReconciliationPolicy,
+    apply_document_reconciliation,
+    load_reconciliation_registry,
+    reconcile_trade_documents,
+)
 from .financial_health import FinancialHealthResult, analyze_financial_health
 from .financial_trends import FinancialTrendResult, analyze_financial_trends
 from .trade_document_assessment import (
@@ -20,11 +29,16 @@ from .trade_document_rules import (
 )
 
 __all__ = [
+    "DocumentComparisonResult",
+    "DocumentReconciliationOutcome",
+    "DocumentReconciliationResult",
     "FinancialHealthResult",
     "FinancialTrendResult",
+    "ReconciliationPolicy",
     "TradeDocumentScreeningOutcome",
     "analyze_financial_health",
     "analyze_financial_trends",
+    "apply_document_reconciliation",
     "apply_trade_document_screening",
     "build_document_risk_signals",
     "build_fatf_country_fact",
@@ -32,6 +46,8 @@ __all__ = [
     "build_world_bank_country_facts",
     "evaluate_trade_document",
     "load_fatf_registry",
+    "load_reconciliation_registry",
     "load_trade_document_rule_registry",
+    "reconcile_trade_documents",
     "reviewed_terms_from_document",
 ]
