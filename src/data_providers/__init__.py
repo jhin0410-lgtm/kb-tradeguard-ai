30343d8ef5e525035ffc23437bd94fc4b1b6b701
jhin0410-lgtm <jhin0410@gmail.com>
2026-07-28 03:09:49 +0900

    Export Korea Customs trade provider

--- src/data_providers/__init__.py
+++ src/data_providers/__init__.py
@@ -7,6 +7,7 @@ from .base import (
 )
 from .bok_ecos import BOKECOSProvider
 from .kexim_fx import KEXIMFXProvider
+from .korea_customs_trade import KoreaCustomsTradeProvider
 from .nts_business import NTSBusinessStatusProvider
 from .opendart import OpenDARTProvider
 from .world_bank_country import WorldBankCountryProvider
@@ -14,6 +15,7 @@ from .world_bank_country import WorldBankCountryProvider
 __all__ = [
     "BOKECOSProvider",
     "KEXIMFXProvider",
+    "KoreaCustomsTradeProvider",
     "NTSBusinessStatusProvider",
     "OpenDARTProvider",
     "ProviderConfigurationError",
